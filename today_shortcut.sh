86fb10c5415af5828109a6937866e54669b3bd83
Dehyrf <Dehyrf@users.noreply.github.com>
2015-09-20 19:04:04 -0500

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	renamed:    append_today.sh -> today_shortcut.sh

--- today_shortcut.sh
+++ today_shortcut.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 touch ~/.bashrc
-echo "alias today='date +"%A, %B %-d, %Y"'" >> ~/.bashrc
+echo "alias today='date'" >> ~/.bashrc
 
 echo Please log out and log back in, then use 'today' to display the date
